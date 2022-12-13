783ff31f36029b44f27b97d4afa7373233ac943c
Javabender333 <mills.oshinkpa@gmail.com>
2022-12-13 23:07:27 +0000

    this is the first commit for Task 0 of the variables project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar followed by a new line
+ * 
+ * Result: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return(0);
+
+}
