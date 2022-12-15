88c26cbf68541ef062ecadb8faee2d722743c8e6
Javabender333 <mills.oshinkpa@gmail.com>
2022-12-15 23:03:59 +0000

    Task 0 done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 98;)
-        positive_or_negative(i);
+	for (i = 98;)
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
