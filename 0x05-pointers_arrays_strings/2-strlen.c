e31e9ee1b82d5d30e891392acd9afbf8fe795f47
Javabendder333 <mills.oshinkpa@gmail.com>
2023-05-27 00:03:31 -0700

    pointers arrays and strings task commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen - Return the length of input string
+ * s: string to be used as input
+ * Return : length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+	return (length);
+}
