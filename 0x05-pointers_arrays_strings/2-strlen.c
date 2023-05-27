c77360b8539958856359efa6cf3f4196e726a28b
Javabendder333 <mills.oshinkpa@gmail.com>
2023-05-27 00:15:45 -0700

    pointers strings arrays task commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 /**
  * _strlen - Return the length of input string
- * s: string to be used as input
- * Return : length of the string
+ * @s: string to be used as input
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
